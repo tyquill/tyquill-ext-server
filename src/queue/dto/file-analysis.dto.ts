819eb2adf84d3d4128f29083b45766a3b104a4d1
qyinm <qusseun@gmail.com>
2025-09-05 19:28:40 +0900

    feat(file-analysis): 파일 분석 요청 DTO에 유효성 검사 추가

--- src/queue/dto/file-analysis.dto.ts
+++ src/queue/dto/file-analysis.dto.ts
@@ -1,3 +1,5 @@
+import { IsNumber, IsUrl, IsString, IsMimeType, IsDateString } from "class-validator";
+
 export interface FileAnalysisMessage {
   uploadedFileId: number;
   fileUrl: string;
@@ -16,11 +18,22 @@ export interface FileAnalysisResponse {
 }
 
 export class FileAnalysisRequestDto {
+  @IsNumber()
   uploadedFileId: number;
+
+  @IsUrl()
   fileUrl: string;
+
+  @IsString()
   fileName: string;
+  
+  @IsMimeType()
   mimeType: string;
+
+  @IsNumber()
   userId: number;
+
+  @IsDateString()
   timestamp: string;
 
   constructor(data: FileAnalysisMessage) {
